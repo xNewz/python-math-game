553a8d6d2964878421b3f4fe2300a1cd1d40074b
xNewz <pargornruasijan@gmail.com>
2021-11-30 20:30:04 +0700

    Add variable operators

--- math_game.py
+++ math_game.py
@@ -1,2 +1,10 @@
 import random
 import operator
+
+def random_problem():
+    operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+    }
