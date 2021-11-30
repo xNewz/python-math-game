259027dbb6631b49b4435c12374bebb64d349d73
xNewz <pargornruasijan@gmail.com>
2021-11-30 20:39:19 +0700

    Add function game and call func

--- math_game.py
+++ math_game.py
@@ -20,3 +20,16 @@ def ask_question():
     answer = random_problem()
     guess = float(input('Enter you answer: '))
     return guess == answer
+
+def game():
+    score = 0
+    while True:
+        if ask_question() == True:
+            score += 1
+            print('Correct !')
+        else:
+            print('Incorrect')
+            break
+    print(f'======== Game Over ========\nYou score is {score}\nKepp going!')
+
+game()
